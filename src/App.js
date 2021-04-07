ee007971c71be8e93478c18350bb8d26602433b6
saiful2612 <bristykodom@gmail.com>
2021-04-07 15:26:28 +0600

    routing issue solved

--- src/App.js
+++ src/App.js
@@ -3,46 +3,11 @@ import './App.css';
 import Header from './Component/Header/Header';
 import Body from './Component/Body/Body';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import About from './Component/About/About';
-import Work from './Component/Work/Work';
-import Contact from './Component/Contact/Contact';
-import Home from './Component/Home/Home';
-import Blog from './Component/Blog/Blog';
-
 function App() {
   return (
     <div>
       <Header></Header>
       <Body></Body>
-
-      <Router>
-        <Switch>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/about">
-            <About></About>
-          </Route>
-          <Route path="/work">
-            <Work></Work>
-          </Route>
-          <Route path="/contact">
-            <Contact></Contact>
-          </Route>
-          <Route path="/blog">
-            <Blog></Blog>
-          </Route>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-        </Switch>
-      </Router>
     </div>
   );
 }
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,16 +1,49 @@
 import React from 'react';
-import "./Header.css"
+import "./Header.css";
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import Home from '../Home/Home';
+import About from '../About/About';
+import Work from '../Work/Work';
+import Contact from '../Contact/Contact';
+import Blog from '../Blog/Blog';
 
 const Header = () => {
     return (
         <div className="header">
-            <nav>
-                <a href="/home">Home</a>
-                <a href="/about">About</a>
-                <a href="/work">Work</a>
-                <a href="/contact">Contact</a>
-                <a href="/blog">Blog</a>
-            </nav>
+            <Router>
+                <nav>
+                    <ul>
+                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/work">Work</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/home">
+                        <Home></Home>
+                    </Route>
+                    <Route path="/about">
+                        <About></About>
+                    </Route>
+                    <Route path="/work">
+                        <Work></Work>
+                    </Route>
+                    <Route path="/contact">
+                        <Contact></Contact>
+                    </Route>
+                    <Route path="/blog">
+                        <Blog></Blog>
+                    </Route>
+                </Switch>
+            </Router>
+
         </div>
     );
 };
