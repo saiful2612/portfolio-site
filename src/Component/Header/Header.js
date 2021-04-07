8656e28da235cd6d896ff970885795cccbc24218
saiful2612 <bristykodom@gmail.com>
2021-04-07 15:53:13 +0600

    navbar fixed

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -23,6 +23,7 @@ const Header = () => {
                         <li><Link to="/about">About</Link></li>
                         <li><Link to="/work">Work</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
+                        <li><Link to="/blog">Link</Link></li>
                     </ul>
                 </nav>
                 <Switch>
