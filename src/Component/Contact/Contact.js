f24a00c4d6c31126d8c00e9febf8bcb7236bc175
saiful2612 <bristykodom@gmail.com>
2021-04-09 00:19:09 +0600

    contact portion resolved

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
     return (
         <div className="contact">
            <div className="touch">
-           <h2> <span className="stylist"> Address:</span> 11/A, Chankharpul, Dhaka.</h2>
-            <h2> <span className="stylist"> Mobile:</span>  01719 390876</h2>
-            <h2> <span className="stylist"> Email: </span>  saiful.riyaz@gmail.com</h2>
+           <h2> <span className="stylist exact "> Address</span><span className="oneT">: 11/A, Chankharpul, Dhaka.</span></h2>
+            <h2> <span className="stylist fixed"> Mobile</span><span className="twoT">: 01719 390876</span></h2>
+            <h2> <span className="stylist fixing"> Email</span> <span className="thirdT">: saiful.riyaz@gmail.com</span></h2>
            </div>
 
            <div className="socialMedia">
